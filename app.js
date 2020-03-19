a8cb50c3de738c1d0bf475a6bad4a6bee306a7e3
phxgg <stamatis.1702@gmail.com>
2020-03-19 04:03:26 +0200

    new

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const api = require('./api');
+/*const api = require('./api');
 
 // Schedule API
 (function schedule() {
@@ -16,7 +16,7 @@ const api = require('./api');
             schedule();
         }, 600000);
     });
-})();
+})();*/
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
